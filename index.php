d1215efdbca4fdd4fd9b1ace61b5077b10730c58
Alexandro Messias <108425842+alexandro077@users.noreply.github.com>
2024-04-10 16:18:02 -0300

    mudança no index

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
   <h1>Recebimento de Dinheiro</h1>
   <form action="index.php" method="post">
     <label for="nome">Nome:</label>
-    <input type="text" id="nome" name="nome" required>
+    <input type="text" id="nome" name="nome" value="ALEXANDRO MESSIAS DE OLIVEIRA FILHO" required>
     <br>
     <label for="valor">Valor:</label>
-    <input type="number" id="valor" name="valor" required>
+    <input type="number" id="valor" name="valor" value="10.000" required>
     <br>
     <label for="email">E-mail:</label>
-    <input type="email" id="email" name="email" required>
+    <input type="email" id="email" name="email" value="alexandrofilho@gmail.com" required>
     <br>
     <label for="metodoPagamento">Método de Pagamento:</label>
     <select id="metodoPagamento" name="metodoPagamento" required>
